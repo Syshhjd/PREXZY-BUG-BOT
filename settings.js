552cb84f1817d2b17bb869f39bb792bafd005e7c
Syshhjd <166198244+Syshhjd@users.noreply.github.com>
2024-09-26 19:48:05 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "+17165036616"
+global.ownername = "MARC-TECH👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
-global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '+17165036616'  //creator number
+global.ownername = 'MARC-TECH👑' //owner name
+global.botname = 'MRC ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +263717750825'
+global.packname = 'EMPEROR SUKUNA 
+global.author = 'PREXZY👑\n\nContact: +17165036616'
 
 //console view/theme
 global.themeemoji = '🪀'
